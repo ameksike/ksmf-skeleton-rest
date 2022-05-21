690a4f75d1cbcb3f6350bb76278af36652537084
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-05-21 16:39:25 -0400

    update comments

--- src/app/validator/Validator.js
+++ src/app/validator/Validator.js
@@ -1,5 +1,13 @@
+/*
+ * @author		Antonio Membrides Espinosa
+ * @email		tonykssa@gmail.com
+ * @date		19/04/2022
+ * @copyright  	Copyright (c) 2020-2030
+ * @license    	GPL
+ * @version    	1.0 
+ * @require     express-validator
+ * */
 const { validationResult } = require('express-validator');
-
 const validate = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {

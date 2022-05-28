8880498ad7a5d1950a01b0d84d5af69ba76b3758
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-05-28 13:56:58 -0400

    update docs

--- src/comment/doc/paths/CommentController.js
+++ src/comment/doc/paths/CommentController.js
@@ -3,6 +3,48 @@ module.exports = {
         "get": {
             "tags": ["COMMENT"],
             "description": "Returns all comments from the system",
+            "parameters": [
+                {
+                    "name": "page",
+                    "in": "path",
+                    "description": "Number of data page to load, by default is 0",
+                    "required": false,
+                    "schema": {
+                        "type": "integer"
+                    },
+                    "example": 0
+                },
+                {
+                    "name": "size",
+                    "in": "path",
+                    "description": "Number records to load by page, by default is 10",
+                    "required": false,
+                    "schema": {
+                        "type": "string"
+                    },
+                    "example": 10
+                },
+                {
+                    "name": "sort",
+                    "in": "path",
+                    "description": "List of fields by which you want to order, for each one the type of ordering is specified ['ASC', 'DESC']",
+                    "required": false,
+                    "schema": {
+                        "type": "string"
+                    },
+                    "example": '[["id", "DESC"]]'
+                },
+                {
+                    "name": "filter",
+                    "in": "path",
+                    "description": "Object composed of one or more search criteria, each property represents a criteria based on equality with the associated value",
+                    "required": false,
+                    "schema": {
+                        "type": "string"
+                    },
+                    "example": '[["flightId", [666, 222], "in"],["date", "2022-05-27", "gte"]]'
+                }
+            ],
             "responses": {
                 "200": {
                     "description": "A list of comment.",
